be40004fd46a87e5ac73428da3474c788ce87c33
priyanksutaria <142902108+priyanksutaria@users.noreply.github.com>
2024-04-01 18:33:07 +0530

    fixed

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './screens/Home';
-
-const Stack = createNativeStackNavigator();
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false}}>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello, RN!</Text>
+    </View>
   );
 }
+
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Home from './screens/Home';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator screenOptions={{ headerShown: false}}>
+//         <Stack.Screen name="Home" component={Home} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
